1cad9f2e6b8f037d58621544461a6ee1afd594ce
yiyi <yansuofengyue@icloud.com>
2020-05-27 07:16:16 +0800

    t添加redis缓存

--- src/main/java/com/yicloud/trans/controller/DrugRegionAliasController.java
+++ src/main/java/com/yicloud/trans/controller/DrugRegionAliasController.java
@@ -7,9 +7,12 @@ import com.yicloud.trans.service.mysql.DrugRegionAliasService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -26,11 +29,24 @@ public class DrugRegionAliasController {
     private static final Logger LOGGER = LoggerFactory.getLogger(PatientsController.class);
 
     @Autowired
+    private RedisTemplate<String, Serializable> redisCacheTemplate;
+
+    @Autowired
     private DrugRegionAliasService drugRegionAliasService;
 
     @RequestMapping("/list")
     public List<DrugRegionAlias> drugRegionAliasList(String drgState){
-        List<DrugRegionAlias> list = drugRegionAliasService.list(new QueryWrapper<DrugRegionAlias>().lambda().eq(DrugRegionAlias::getDrgType, "1").eq(DrugRegionAlias::getDrgState,drgState));
+        List<DrugRegionAlias> list=new ArrayList<>(2048);
+        if (redisCacheTemplate.opsForSet().size("drugRegionAlias")>0){
+            list = (List<DrugRegionAlias>) redisCacheTemplate.opsForSet().pop("drugRegionAlias",redisCacheTemplate.opsForSet().size("drugRegionAlias"));
+             list = drugRegionAliasService.list(new QueryWrapper<DrugRegionAlias>().lambda().eq(DrugRegionAlias::getDrgType, "1").eq(DrugRegionAlias::getDrgState,drgState));
+        }else {
+            list = drugRegionAliasService.list(new QueryWrapper<DrugRegionAlias>().lambda().eq(DrugRegionAlias::getDrgType, "1").eq(DrugRegionAlias::getDrgState,drgState));
+            for (DrugRegionAlias drugRegionAlias:list){
+                redisCacheTemplate.opsForSet().add("drugRegionAlias",drugRegionAlias);
+            }
+        }
+
         return list;
     }
 }
